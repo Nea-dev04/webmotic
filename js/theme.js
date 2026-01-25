5bfad96444a528b9b4ab999b9a3067aa056471d5
Nea-dev04 <webmoticnea@gmail.com>
2026-01-25 19:24:01 +0300

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+}
+
+if (toggle) {
+  toggle.onclick = () => {
+    body.classList.toggle("dark");
+    localStorage.setItem(
+      "theme",
+      body.classList.contains("dark") ? "dark" : "light"
+    );
+  };
+}
